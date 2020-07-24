1427ca9cfe45a41713eb85e1dd6c7324df87b37e
Ranjit Jhala <jhala@cs.ucsd.edu>
2020-07-24 16:48:46 -0700

    restore package.yaml.liquid

--- Main.hs
+++ Main.hs
@@ -5,17 +5,6 @@ import System.Directory (listDirectory)
 import System.FilePath (addExtension)
 import System.FilePath ((</>))
 
-import Data.Char (toUpper)
-import System.Directory (renameFile, getTemporaryDirectory)
-import System.Environment (getArgs)
-
--- main = do
---     [file] <- getArgs
---     tmpDir <- getTemporaryDirectory
---     let tmpFile = tmpDir ++ "/" ++ file
---     readFile file >>= writeFile tmpFile . map toUpper
---     renameFile tmpFile file
-
 main :: IO ()
 main = hideIgnores -- return ()
 
