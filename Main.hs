aa4ec009b084ef02c5e9a87f4f8dce9cce1a6b2b
Ranjit Jhala <jhala@cs.ucsd.edu>
2020-07-24 16:18:06 -0700

    about to hide

--- Main.hs
+++ Main.hs
@@ -2,20 +2,36 @@ module Main where
 
 import qualified Data.List as L 
 import System.Directory (listDirectory)
+import System.FilePath (addExtension)
+import System.FilePath ((</>))
+
+import Data.Char (toUpper)
+import System.Directory (renameFile, getTemporaryDirectory)
+import System.Environment (getArgs)
+
+-- main = do
+--     [file] <- getArgs
+--     tmpDir <- getTemporaryDirectory
+--     let tmpFile = tmpDir ++ "/" ++ file
+--     readFile file >>= writeFile tmpFile . map toUpper
+--     renameFile tmpFile file
 
 main :: IO ()
 main = hideIgnores -- return ()
 
 hideIgnores :: IO ()
 hideIgnores = do
-  files <- listDirectory "src/"
-  mapM hideIgnore files
+  tmpDir <- getTemporaryDirectory
+  files <- filter (L.isSuffixOf ".lhs") <$> listDirectory "src/"
+  mapM (hideIgnore tmpDir . ("src" </>)) files
   return ()
 
-hideIgnore :: FilePath -> IO ()
-hideIgnore f = do
+hideIgnore :: FilePath -> FilePath -> IO ()
+hideIgnore tmpDir f = do
+    let tmpFile = tmpDir </> f
     str <- unlines . map hide . lines <$> readFile f
-    writeFile f str
+    writeFile tmpFile str
+    renameFile tmpFile file
 
 hide :: String -> String
 hide str 
